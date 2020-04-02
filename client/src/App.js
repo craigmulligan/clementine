7342805334176632b7c1309f7f7aba606d6023b1
Craig Mulligan <hobochildster@gmail.com>
2020-04-02 13:04:43 +0200

    init viz

--- client/src/App.js
+++ client/src/App.js
@@ -8,72 +8,72 @@ import { Route, Switch } from 'wouter'
 import { UserProvider, UserRedirect } from './user'
 import Menu from './menu'
 import { TraceList } from './trace'
+import { TimeLine } from './timeline'
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <UserProvider>
-        <body>
-          <main>
-            <Menu />
-            <Route path="/signup" component={Signup} />
-            <Route path="/login" component={Login} />
-            <UserRedirect>
-              <Route path="/graph" component={GraphList} />
-              <Switch>
-                <Route
-                  path="/graph/create"
-                  component={({ params }) => <GraphCreate />}
+        <main>
+          <Menu />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <UserRedirect>
+            <Route path="/graph" component={GraphList} />
+            <Switch>
+              <Route
+                path="/graph/create"
+                component={({ params }) => <GraphCreate />}
+              />
+              <Route
+                path="/graph/:graphId"
+                component={({ params }) => (
+                  <GraphShow graphId={params.graphId} />
+                )}
+              />
+            </Switch>
+            <Route
+              path="/graph/:graphId/settings"
+              component={({ params }) => (
+                <GraphSettings graphId={params.graphId} />
+              )}
+            />
+            <Route
+              path="/graph/:graphId/operation"
+              component={({ params }) => (
+                <OperationList graphId={params.graphId} />
+              )}
+            />
+            <Route
+              path="/graph/:graphId/timeline"
+              component={({ params }) => <TimeLine graphId={params.graphId} />}
+            />
+            <Route
+              path="/graph/:graphId/operation/:operationId"
+              component={({ params }) => (
+                <OperationShow
+                  graphId={params.graphId}
+                  operationId={params.operationId}
                 />
-                <Route
-                  path="/graph/:graphId"
-                  component={({ params }) => (
-                    <GraphShow graphId={params.graphId} />
-                  )}
+              )}
+            />
+            <Route
+              path="/graph/:graphId/operation/:operationId/source"
+              component={({ params }) => (
+                <OperationSource>{atob(params.operationId)}</OperationSource>
+              )}
+            />
+            <Route
+              path="/graph/:graphId/operation/:operationId/trace"
+              component={({ params }) => (
+                <TraceList
+                  graphId={params.graphId}
+                  operationId={params.operationId}
                 />
-              </Switch>
-              <Route
-                path="/graph/:graphId/settings"
-                component={({ params }) => (
-                  <GraphSettings graphId={params.graphId} />
-                )}
-              />
-              <Route
-                path="/graph/:graphId/operation"
-                component={({ params }) => (
-                  <OperationList
-                    graphId={params.graphId}
-                    operationId={params.operationId}
-                  />
-                )}
-              />
-              <Route
-                path="/graph/:graphId/operation/:operationId"
-                component={({ params }) => (
-                  <OperationShow
-                    graphId={params.graphId}
-                    operationId={params.operationId}
-                  />
-                )}
-              />
-              <Route
-                path="/graph/:graphId/operation/:operationId/source"
-                component={({ params }) => (
-                  <OperationSource>{atob(params.operationId)}</OperationSource>
-                )}
-              />
-              <Route
-                path="/graph/:graphId/operation/:operationId/trace"
-                component={({ params }) => (
-                  <TraceList
-                    graphId={params.graphId}
-                    operationId={params.operationId}
-                  />
-                )}
-              />
-            </UserRedirect>
-          </main>
-        </body>
+              )}
+            />
+          </UserRedirect>
+        </main>
       </UserProvider>
     </ApolloProvider>
   )
