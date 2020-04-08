23f0085deb10cae1990adc9210b43df3dfc60fa9
Craig Mulligan <hobochildster@gmail.com>
2020-04-08 19:29:04 +0200

    get started on custom filter component

--- client/src/trace/filters.js
+++ client/src/trace/filters.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
 import { Loading, ErrorBanner } from '../utils'
@@ -16,6 +16,23 @@ const TRACE_FILTER_OPTIONS = gql`
   }
 `
 
+const autoComplete = ({ setSuggestions, fields }) => evt => {
+  const value = evt.target.value
+  const trigger = ':'
+
+  const matches = value.match(/\w+:?(\w+)?/g)
+
+  if (matches) {
+    const lastMatch = matches.pop()
+
+    if (!lastMatch.includes(trigger)) {
+      setSuggestions(fields.map(x => x.label).filter(x => x.includes(lastMatch)))
+    } else {
+      setSuggestions([])
+    }
+  }
+}
+
 export default function TraceFilters({ graphId, onChange }) {
   const {
     conditions,
@@ -23,6 +40,7 @@ export default function TraceFilters({ graphId, onChange }) {
     setFilterInterval,
     filterInterval
   } = useContext(FiltersContext)
+  const [suggestions, setSuggestions] = useState([])
   const { loading, error, data } = useQuery(TRACE_FILTER_OPTIONS, {
     variables: {
       graphId
@@ -44,9 +62,7 @@ export default function TraceFilters({ graphId, onChange }) {
       return {
         label: k,
         name: k,
-        type: 'list',
-        operators: ['eq', 'ne'],
-        list: v
+        values: v
           ? v
               .filter(v => !!v)
               .map(v => ({
@@ -59,23 +75,30 @@ export default function TraceFilters({ graphId, onChange }) {
 
   return (
     <div className={styles.wrapper}>
-      <select
-        value={filterInterval}
-        onChange={v => {
-          setFilterInterval(v.target.value)
-        }}
-      >
-        <option value="hour">Last hour</option>
-        <option value="day">Last day</option>
-        <option value="month">Last month</option>
-      </select>
-      <VisualFilter
-        conditions={conditions}
-        fields={fields}
-        onChange={data => {
-          setFilters(data)
-        }}
-      />
+      <input type="text" onChange={autoComplete({ setSuggestions, fields })} />
+      <div>{
+      suggestions.map((suggestion) => {
+        return (<li key={suggestion}>{suggestion}</li>)
+      })
+      }</div>
     </div>
   )
 }
+
+// <select
+// value={filterInterval}
+// onChange={v => {
+// setFilterInterval(v.target.value)
+// }}
+// >
+// <option value="hour">Last hour</option>
+// <option value="day">Last day</option>
+// <option value="month">Last month</option>
+// </select>
+// <VisualFilter
+// conditions={conditions}
+// fields={fields}
+// onChange={data => {
+// setFilters(data)
+// }}
+// />
