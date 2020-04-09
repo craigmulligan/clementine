e96dc5ee862f5f6c79ef9ac1f4a80c92e6d416f2
Craig Mulligan <hobochildster@gmail.com>
2020-04-09 12:39:40 +0200

    kinda

--- client/src/trace/filters.js
+++ client/src/trace/filters.js
@@ -1,10 +1,8 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import React, { useContext, useState, useEffect, memo } from 'react'
 import { gql } from 'apollo-boost'
-import { Loading, ErrorBanner } from '../utils'
-import VisualFilter from 'react-visual-filter'
 import styles from './filters.module.css'
 import FiltersContext from './filtersContext'
+import client from '../client'
 
 const TRACE_FILTER_OPTIONS = gql`
   query traceFilterOptions($graphId: ID!) {
@@ -16,68 +14,157 @@ const TRACE_FILTER_OPTIONS = gql`
   }
 `
 
-const autoComplete = ({ setSuggestions, fields, trigger }) => value => {
-  const matches = value.match(/\w+:?(\S*)?/g)
-  if (matches) {
-    const lastMatch = matches.pop()
-    if (!lastMatch.includes(trigger)) {
-      setSuggestions(fields.map(x => x.label).filter(x => x.startsWith(lastMatch)))
-    } else {
-      const [fieldLabel, valueName] = lastMatch.split(':')
-      const field = fields.find(f => f.label === fieldLabel)
-      if (field) {
-        const isComplete =  field.values.map(x => x.label).find(x => {
-          return x === valueName
-        })
+const autoComplete = ({ setSuggestions, fields, trigger, setValue }) => {
+  const matcher = value => value.match(/\w+:?(\S*)?/g)
 
-        if (isComplete) {
-          setSuggestions(fields.map(x => x.label))
-        } else {
-          setSuggestions(field.values.map(x => x.label).filter(x => {
-            return x.startsWith(valueName)
-          }))
+  const completer = value => {
+    const matches = matcher(value)
+    if (matches) {
+      const lastMatch = matches.pop()
+      if (!lastMatch.includes(trigger)) {
+        setSuggestions(fields.map(x => x.label).filter(x => x.startsWith(lastMatch)))
+      } else {
+        const [fieldLabel, valueName] = lastMatch.split(':')
+        const field = fields.find(f => f.label === fieldLabel)
+        if (field) {
+          const isComplete =  field.values.map(x => x.label).find(x => {
+            return x === valueName
+          })
+
+          if (isComplete) {
+            setSuggestions(fields.map(x => x.label))
+          } else {
+            setSuggestions(field.values.map(x => x.label).filter(x => {
+              return x.startsWith(valueName)
+            }))
+          }
         }
       }
+    } else {
+      setSuggestions(fields.map(x => x.label))
     }
-  } else {
-    setSuggestions(fields.map(x => x.label))
   }
+
+  const selector = (value, suggestion) => {
+    let v
+    const matches = matcher(value)
+      if (matches) {
+        // TODO double selecting suggestion there is a bug.
+        const lastMatch = matches.pop()
+        if (lastMatch.includes(trigger)) {
+          const [fieldLabel, valueName] = lastMatch.split(':')
+          const field = fields.find(f => f.label === fieldLabel)
+
+          if (field) {
+            const isComplete =  field.values.map(x => x.label).find(x => {
+              return x === valueName
+            })
+            if (isComplete) {
+              // full match move on to next field
+              v = [...matches, lastMatch, suggestion + ":"].join(' ')
+            } else {
+              // not full match replace suggesion
+              v = [...matches, fieldLabel +  ":" + suggestion + " "].join(' ')
+            }
+          }
+
+        } else {
+          v = [matches, suggestion + ":"].join(' ')
+        }
+      } else {
+        v = suggestion + ":"
+      }
+
+
+      setValue(v)
+      completer(v)
+  }
+
+  const completeMatches = (value) => {
+        const matches = matcher(value) || []
+
+        return matches.filter(match => {
+          return match.includes(trigger)
+        }).filter(match => {
+          const [fieldLabel, valueName] = match.split(':')
+          const field = fields.find(f => f.label === fieldLabel)
+
+          if (!field) {
+            return false
+          }
+
+          return field.values.map(x => x.label).find(x => {
+            return x === valueName
+          })
+        }).map((match) => {
+            const [fieldLabel, valueLabel] = match.split(':')
+            const field = fields.find(f => f.label === fieldLabel)
+            const value = field.values.find(v => v.label === valueLabel)
+
+            return {
+               field: field.name,
+               value: valueLabel,
+              operator: 'eq'
+            }
+
+          })
+
+      }
+
+
+  return [completer, selector, completeMatches]
 }
 
 
 export default function TraceFilters({ graphId, onChange }) {
   const {
-    conditions,
     setFilters,
     setFilterInterval,
-    filterInterval
+    filterInterval,
+    filters,
   } = useContext(FiltersContext)
   const [suggestions, setSuggestions] = useState([])
-  const [value, setValue] = useState('')
-  const { loading, error, data } = useQuery(TRACE_FILTER_OPTIONS, {
-    variables: {
-      graphId
+  const [fields, setFields] = useState([])
+
+  const toString = (filters) => {
+    return filters.map(f => {
+      return `${f.field}:${f.value}`
+    }).join(' ')
+  }
+
+  const [value, setValue] = useState(toString(filters))
+  const trigger = ':'
+  const [completer, select, matcher] = autoComplete({ setSuggestions, setValue, fields, trigger, filters })
+  const completeMatches = matcher(value)
+  // completer returns two functions one for
+  // updating suggestions based on value
+  // one for updating the value based on a selected value.
+
+  useEffect(() => {
+    completer(value)
+  }, [value])
+
+  useEffect(() => {
+    if (suggestions.length === 0) {
+      return
     }
-  })
+    const interval = suggestions.filter(f => f.field === 'interval').pop()
 
-  if (loading) return <Loading />
-  if (error) return <ErrorBanner error={error} />
-
-    const intervalField = {
-      name: 'interval', label: 'last', values: [{
-        name: 'hour',
-        label: 'hour'
-      }, {
-        name: 'day',
-        label: 'day'
-      }, {
-        name: 'month',
-        label: 'month'
-      }]
+    if (interval) {
+      setFilterInterval(interval.value)
     }
 
-  // Todo we should just have an arbitrary way to select to - from.
-  const apiFields = Object.entries(data.traceFilterOptions)
+    const stdFilters = completeMatches.filter(f => f.field !== 'interval')
+    setFilters(stdFilters)
+  }, [completeMatches.length])
+
+  useEffect(() => {
+    client.query({ query: TRACE_FILTER_OPTIONS,
+      variables: {
+        graphId
+      }
+    }).then(({ data }) => {
+    const apiFields = Object.entries(data.traceFilterOptions)
     .filter(([k, v]) => {
       if (k === '__typename') {
         return false
@@ -99,53 +186,34 @@ export default function TraceFilters({ graphId, onChange }) {
       }
     })
 
+    const intervalField = {
+      name: 'interval', label: 'last', values: [{
+        name: 'hour',
+        label: 'hour'
+      }, {
+        name: 'day',
+        label: 'day'
+      }, {
+        name: 'month',
+        label: 'month'
+      }]
+    }
 
-  const fields = [intervalField, ...apiFields]
-  const trigger = ':'
-  const completer = autoComplete({ setSuggestions, fields, trigger })
+    setFields([intervalField, ...apiFields])
+    })
+  }, [graphId])
 
   return (
     <div className={styles.wrapper}>
-    <div><textarea type="text" value={value} onChange={(evt) => {
+    <div><textarea onSelect={(evt) => {
+      completer(value)
+    }} type="text" value={value} onChange={(evt) => {
       setValue(evt.target.value)
       completer(evt.target.value)
     }} /></div>
       <div className={styles.suggestions}>{
       suggestions.map((suggestion) => {
-        return (<div className={styles.suggestion} onClick={() => {
-        const trigger = ':'
-        let v
-        const matches = value.match(/\w+:?(\S*)?/g)
-          if (matches) {
-            // TODO double selecting suggestion there is a bug.
-            const lastMatch = matches.pop()
-            if (lastMatch.includes(trigger)) {
-              const [fieldLabel, valueName] = lastMatch.split(':')
-              const field = fields.find(f => f.label === fieldLabel)
-
-              if (field) {
-                const isComplete =  field.values.map(x => x.label).find(x => {
-                  return x === valueName
-                })
-                if (isComplete) {
-                  // full match move on to next field
-                  v = [...matches, lastMatch, suggestion + ":"].join(' ')
-                } else {
-                  // not full match replace suggesion
-                  v = [...matches, fieldLabel +  ":" + suggestion + " "].join(' ')
-                }
-              }
-
-            } else {
-              v = [matches, suggestion + ":"].join(' ')
-            }
-          } else {
-            v = suggestion
-          }
-
-          setValue(v)
-          completer(v)
-        }} key={suggestion}>{suggestion}</div>)
+        return (<div className={styles.suggestion} onClick={() => { select(value, suggestion) }} key={suggestion}>{suggestion}</div>)
       })
       }</div>
     </div>
